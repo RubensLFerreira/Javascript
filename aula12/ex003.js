8a0dda0ce0bfeffd082a8fd464fb9d6bcc714460
RubensLFerreira <rubenslima000@gmail.com>
2020-07-29 14:59:54 -0300

    29/07/2020

--- aula12/ex003.js
+++ aula12/ex003.js
@@ -1,25 +1,25 @@
 var agora = new Date()
 var diaSem = agora.getDay()
 
-console.log(diaSem)
+console.log(`Seu dia é "${diaSem}"`)
 switch(diaSem) {
     case 0: 
         console.log('Domingo')
         break
     case 1: 
-        console.log('Segunda')
+        console.log('Segunda-feira')
         break
     case 2: 
-        console.log('Terça')
+        console.log('Terça-feira')
         break
     case 3:
-        console.log('Quarta')
+        console.log('Quarta-feira')
         break
     case 4:
-        console.log('Quinta')
+        console.log('Quinta-feira')
         break
     case 5:
-        console.log('Sexta')
+        console.log('Sexta-feira')
         break
     case 6: 
         console.log('Sábado')
