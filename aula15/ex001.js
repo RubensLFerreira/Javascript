a0c6dcaa0446a6b732b314170704a344f3114192
RubensLFerreira <rubenslima000@gmail.com>
2020-08-06 15:14:02 -0300

    06/08/2020

---
+++ aula15/ex001.js
@@ -0,0 +1,15 @@
+/* let num = [5, 4, 2, 8, 3]
+num.push[7] = 1
+console.log(`Add novo elemento ${num.push(9)}`)
+console.log(`O terceiro elemento é ${num[3]}`)
+console.log(`Meu array tem ${num.length} elementos`)
+console.log(`Todos os elementos ${num}`);
+console.log(`Array em ordem crescente: ${num.sort()}`); */
+
+/* let num = 0
+while(num <= 10) {
+    console.log(num)
+    num++
+} */
+
+    
---
+++ aula15/ex002.js
@@ -0,0 +1,4 @@
+let num = [2, 4, 6, 8, 3, 5];
+for(let pos = 0; pos < pos.lenght; pos++) { 
+    console.log(num[pos])
+}
